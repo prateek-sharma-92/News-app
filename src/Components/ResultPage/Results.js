c578d54ca121e6833c75ba8e19da641e63c44a3a
prateek-sharma-92 <sharma.prateek0410@gmail.com>
2022-04-08 10:02:16 +0530

    added axios in useEffect

--- src/Components/ResultPage/Results.js
+++ src/Components/ResultPage/Results.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { API_key } from "../../Redux/keys";
 import "./Results.css";
@@ -7,16 +7,18 @@ import "./Results.css";
 function Results() {
   const [Newsdata, setData] = useState();
   const category = useSelector((state) => state.reducer);
-  console.log(category);
-
-  axios
-    .get(
-      `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_key}`
-    )
-    .then((res) => {
-      console.log(res.data.articles);
-      setData(res.data.articles);
-    });
+  useEffect(() => {
+    async function fetchData() {
+      await axios
+        .get(
+          `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_key}`
+        )
+        .then((res) => {
+          setData(res.data.articles);
+        });
+    }
+    fetchData();
+  }, [category]);
 
   return Newsdata?.map((item) => (
     <div className="container">
@@ -25,14 +27,19 @@ function Results() {
       </div>
       <div className="text-container">
         <div className="text-title">
-          <a href={item.url}>{item.title}</a>
+          <a href={item.url} rel="noreferrer" target="_blank">
+            {item.title}
+          </a>
         </div>
         <div className="date">
           <b>short</b> by {item.author} / {item.publishedAt}
         </div>
         <div className="description">{item.description}</div>
         <div className="read-more">
-          read more at <a href={item.url}>{item.source.name}</a>
+          read more at{" "}
+          <a href={item.url} rel="noreferrer" target="_blank">
+            {item.source.name}
+          </a>
         </div>
       </div>
     </div>
